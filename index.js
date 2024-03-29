a0f640dabdb928cf889a6a334082fc677a312566
unknown <nafim191119@gmai.com>
2024-03-29 15:05:56 +0600

    items added

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.POST || 5000;
 
@@ -9,21 +8,7 @@ const port = process.env.POST || 5000;
 app.use(cors());
 app.use(express.json());
 
-const verifyIWT = (req, res, next) => {
-    const authorization = req.headers.authorization;
-    if (!authorization) {
-        return res.send(401).send({ error: true, message: 'unauthorized access' });
-    }
-    const token = authorization.split(' ')[1];
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-        if(err){
-            return res.status(401).send({error: true, message: 'unauthorized access'})
-        }
-        req.decoded = decoded;
-        next();
-    })
-}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -46,13 +31,6 @@ async function run() {
         const cartCollection = client.db("dishHub").collection("carts");
 
 
-        app.post('/jwt', (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-
-            res.send({ token });
-        })
-
         app.get('/users', async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
@@ -69,18 +47,6 @@ async function run() {
             res.send(result);
         })
 
-        app.patch('/users/admin/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-
-            const updateDoc = {
-                $set: {
-                    role: 'admin'
-                },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
-        })
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
@@ -91,16 +57,8 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/carts', verifyIWT,  async (req, res) => {
+        app.get('/carts', async (req, res) => {
             const email = req.query.email;
-            if (!email) {
-                res.send([])
-            }
-
-            const decodedEmail = req.decoded.email;
-            if(email !== decodedEmail){
-                return res.send(401).send({error: true, message: 'unauthorized access'});
-            }
             const query = { email: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result);
